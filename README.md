188140202a35e806b6d64e3c8d2151ffc1bb51a1
infojav <javier.perea@outlook.com>
2015-06-29 10:02:42 +0100

    some changes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # vagrant-setup
+
+This is a simple vagrant setup.  
+
+## Packages Included
+
+  - Ubuntu Trusty64
+  - Ngnix
+  - nodejs
+  - Php
+  - composer
+  - Mysql
+  - Postgresql
+  - mongodb
+  - redis
+  - memcache
+  - selenium
+  - etc..
--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,39 +1,56 @@
 #!/usr/bin/env bash
 
+clear
 sudo apt-get update
 
 # ------- Ngnix ------
-echo "@jav: -----> Installing Ngnix"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing Ngnix"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y nginx
 
 # ------- Curl -------
-echo "@jav: -----> Installing Curl"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing Curl"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y curl
 
 # ------- Git --------
-echo "@jav: -----> Installing git"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing git"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y git
 
 # ------- Redis ------
-echo "@jav: -----> Installing redis"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing redis"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y redis-server
 sudo apt-get install -y redis-cli
 
 # ------- MongoDB ----
-echo "@jav: -----> Installing mongodb"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing mongodb"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y mongodb
 
 
 # ------- Memcached --
-echo "@jav: -----> Installing Memcached"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing Memcached"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y memcached
 
 # ------- PHP5 -------
-echo "@jav: -----> Installing PHP5"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing PHP5"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y php5 php5-common php5-cli php5-fpm php5-intl php5-imagick php5-pgsql php5-gd php5-mcrypt php5-memcached php5-curl
 
 # ------- PHP5 x-debug
-echo "@jav: -----> Installing and setting xdebug for PHP"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing and setting xdebug for PHP"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y php5-xdebug
 
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
@@ -43,24 +60,35 @@ xdebug.show_local_vars=1
 EOF
 
 # ------- Composer ---
-echo "@jav: -----> Composer"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Composer"
+echo "-----------------------------------------------------------------------------"
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 # ------- Install Java
-echo "@jav: -----> Java"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Java"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install default-jre
-#udo apt-get install default-jdk
+#sudo apt-get install default-jdk
 
 # ------- Install Xvfb - X Virtual Frame Buffer
 # For webdriver headless
-echo "@jav: -----> Installing Xvfb"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing Xvfb"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y xvfb
 sudo apt-get install -y xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic 
 sudo apt-get install -y x11-xkb-utils xserver-xorg-core dbus-x11
 sudo apt-get install -y libfontconfig1-dev
 
-sudo mv /vagrant/setup/xvfb /etc/init.d/xvfb
+xvfbFile = /vagrant/setup/xvfb
+if [-f $xvfbFile]; then
+	sudo cp $xvfbFile /etc/init.d/xvfb
+else
+	printf '\n%s does not exist!\n' "$xvfbFile"
+fi
 sudo chmod a+x /etc/init.d/xvfb
 sudo update-rc.d xvfb defaults
 
@@ -72,7 +100,9 @@ EOF
 sudo apt-get install -y chromium-browser firefox
 
 # ------- nodejs -----
-echo "@jav: -----> Installing nodejs"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing nodejs"
+echo "-----------------------------------------------------------------------------"
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get purge -y node nodejs nodejs-legacy npm
@@ -80,7 +110,9 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
 # ------- some npms
-echo "@jav: -----> Installing some npms"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Installing some npms"
+echo "-----------------------------------------------------------------------------"
 sudo npm install -g bower
 sudo npm install -g grunt
 sudo npm install -g gulp
@@ -92,17 +124,30 @@ sudo npm install -g phantomjs
 sudo npm install -g chromedriver
 
 # ------- Selenium
-echo "@jav: ------> Installing selenium"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: ------> Installing selenium"
+echo "-----------------------------------------------------------------------------"
 sudo /usr/sbin/useradd -m -s /bin/bash -d /home/selenium selenium
 sudo mkdir /usr/local/share/selenium
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.37.0.jar
-sudo mv selenium-server-standalone-2.37.0.jar /usr/local/share/selenium
+seleniumServerFile = selenium-server-standalone-2.37.0.jar
+if [-f $seleniumServerFile]; then
+	sudo cp $seleniumServerFile /usr/local/share/selenium
+else
+	printf '\n%s does not exist!\n' "$seleniumServerFile"
+fi
 sudo chown -R selenium:selenium /usr/local/share/selenium
 
 sudo mkdir /var/log/selenium
 sudo chown selenium:selenium /var/log/selenium
 
-sudo mv /vagrant/setup/selenium /etc/init.d/selenium
+seleniumFile = /vagrant/setup/selenium
+if [ -f $seleniumFile ]; then
+	sudo cp $seleniumFile /etc/init.d/selenium
+else
+	printf '\n%s does not exist!\n' "$seleniumFile"
+fi
+
 sudo chown root:root /etc/init.d/selenium
 sudo chmod a+x /etc/init.d/selenium
 sudo update-rc.d selenium defaults
@@ -111,40 +156,47 @@ sudo touch /phantomjsdriver.log
 sudo chmod 666 /phantomjsdriver.log
 
 # -------- Imagemagick
-echo "@jav: ------> Installing imagemagick"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: ------> Installing imagemagick"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install -y imagemagick
 
-# ------- Postgress ---
+# -------- Postgress ---
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: ------> Installing portgresql"
+echo "-----------------------------------------------------------------------------"
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main 9.4" >> pgdg.list
 sudo cp pgdg.list /etc/apt/sources.list.d/pgdg.list
 sudo apt-get update
 sudo apt-get -y install postgresql-9.4 postgresql-client-9.4
 
-echo "@jav: -----> Changing to dummy password"
+echo "@infojav: -----> Changing to dummy password"
 sudo pg_createcluster 9.4 test 
 sudo vi /etc/postgresql/9.4/test/pg_hba.conf 
 sudo -u postgres psql postgres -c "ALTER USER postgres WITH ENCRYPTED PASSWORD 'postgres'"
 # Edit to allow socket access, not just local unix access
-echo "@jav: -----> Patching pg_hba to change -> socket access"
+echo "@infojav: -----> Patching pg_hba to change -> socket access"
 sudo cp /etc/postgresql/9.4/test/pg_hba.conf .
 sudo chmod a+rw pg_hba.conf
 sudo sed 's/local.*all.*postgres.*peer/local all postgres md5/' < pg_hba.conf > pg_hba2.conf
 sudo sed 's/local.*all.*all.*peer/local all all md5/' < pg_hba2.conf > pg_hba3.conf
-echo "@jav: -----> Altered login to use md5 not peer:"
+echo "@infojav: -----> Altered login to use md5 not peer:"
 sudo printf "\nhost    all             all             127.0.0.1/32            trust\n" >> pg_hba3.conf
 
 cat pg_hba3.conf
 
 sudo chmod u-rw pg_hba3.conf 
 sudo cp pg_hba3.conf /etc/postgresql/9.4/test/pg_hba.conf
-echo "@jav: -----> Patching complete, restarting"
+echo "@infojav: -----> Patching complete, restarting"
 
-# -------- Iniciando Servicios
-echo "@jav: -----> Iniciando Servicios"
-echo "@jav: ** nginx **"
+# -------- Starting services
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: -----> Starting services"
+echo "-----------------------------------------------------------------------------"
+echo "@infojav: ** nginx **"
 sudo service nginx restart
-echo "@jav: ** postgresql **"
+echo "@infojav: ** postgresql **"
 sudo /etc/init.d/postgresql restart
 
 
